a63501b5b174739962426a85e35d6c965344f461
Tyler Huynh <tylerhuynh287@gmail.com>
2025-08-18 18:26:11 -0400

    Added an Auth Content to centralize auth state, added a splash screen for in-between states of logging in

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, ActivityIndicator } from "react-native";
+
+export default function SplashScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <ActivityIndicator size="large" />
+        </View>
+    );
+}
